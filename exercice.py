c0ffbe4a86cd0a40bb5c01db9e80f6944cad22e5
jusoue <justine_ouellette@hotmail.com>
2021-10-19 11:39:29 -0400

    exercice 1 fini

--- exercice.py
+++ exercice.py
@@ -2,13 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+import math
+import turtle
 
 # TODO: Définissez vos fonction ici
-
+def volume_ellipsoide(a, b, c, volumic_mass):
+    volume = (4/3) * math.pi * a * b * c
+    mass = volumic_mass * volume
+    return (volume, mass)
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = float(input("c: "))
+    volumic_mass = float(input("Entrez la masse volumique ici: "))
+    print(volume_ellipsoide(a, b, c, volumic_mass))
