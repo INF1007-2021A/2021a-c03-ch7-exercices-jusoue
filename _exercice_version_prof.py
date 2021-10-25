1b983f28690374db9add083c20d99b5138027126
jusoue <justine_ouellette@hotmail.com>
2021-10-25 15:07:48 -0400

    exercice 2 fini

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -4,7 +4,7 @@
 import math
 import sys
 sys.path.insert(1, 'D:\charge_cours\INF1007\H2021\exercices\\2021H_ch6_1_exercices')
-from exercice_ch6 import frequence
+#from exercice_ch6 import frequence
 from turtle import *
 import re
 
@@ -71,6 +71,6 @@ if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     draw_tree()
     print(compute_volume_and_mass())
-    print((lambda sentence: sorted(frequence(sentence), key=frequence(sentence).__getitem__)[-1])("big big test bb"))
+    #print((lambda sentence: sorted(frequence(sentence), key=frequence(sentence).__getitem__)[-1])("big big test bb"))
     check_dna()
 
--- exercice.py
+++ exercice.py
@@ -11,6 +11,23 @@ def volume_ellipsoide(a, b, c, volumic_mass):
     mass = volumic_mass * volume
     return (volume, mass)
 
+def frequence(sentence):
+    letter_dic = {}
+    for letter in sentence:
+        if (letter != " " and letter not in letter_dic):
+            letter_dic[letter] = 0
+        if letter in letter_dic:
+            letter_dic[letter] += 1
+    return letter_dic
+
+def draw_brench():
+
+    return 0
+
+def draw_tree():
+
+    return 0
+
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     a = float(input("a: "))
@@ -18,3 +35,6 @@ if __name__ == '__main__':
     c = float(input("c: "))
     volumic_mass = float(input("Entrez la masse volumique ici: "))
     print(volume_ellipsoide(a, b, c, volumic_mass))
+
+    sentence = "patrick sebastien is a very young and handsome person"
+    print(sorted(frequence(sentence).items(), key = lambda x: x[1])[-1])
