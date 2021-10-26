3c9a6e37f541f93a53715dfa4f368d17f3c785bd
jusoue <justine_ouellette@hotmail.com>
2021-10-25 21:14:08 -0400

    arbre fini mais ca manque de comprehension

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 
 # TODO: Importez vos modules ici
 import math
-import turtle
+from turtle import *
 
 # TODO: Définissez vos fonction ici
 def volume_ellipsoide(a, b, c, volumic_mass):
@@ -20,21 +20,35 @@ def frequence(sentence):
             letter_dic[letter] += 1
     return letter_dic
 
-def draw_brench():
-
-    return 0
+def draw_branch(branch_len, pen_len, angle):
+    if (branch_len > 0 and pen_len > 0):
+        pensize(pen_len)
+        forward(branch_len)
+        right(angle)
+        draw_branch(branch_len - 10, pen_len - 1, angle - 5)
+        left(angle * 2)
+        draw_branch(branch_len - 10, pen_len - 1, angle - 5)
+        right(angle)
+        back(branch_len)
 
 def draw_tree():
-
-    return 0
+    setheading(90)
+    color("brown")
+    draw_branch(70, 7, 35)
+    done()
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    a = float(input("a: "))
-    b = float(input("b: "))
-    c = float(input("c: "))
-    volumic_mass = float(input("Entrez la masse volumique ici: "))
-    print(volume_ellipsoide(a, b, c, volumic_mass))
+    #a = float(input("a: "))
+    #b = float(input("b: "))
+    #c = float(input("c: "))
+    #volumic_mass = float(input("Entrez la masse volumique ici: "))
+    #print(volume_ellipsoide(a, b, c, volumic_mass))
 
-    sentence = "patrick sebastien is a very young and handsome person"
-    print(sorted(frequence(sentence).items(), key = lambda x: x[1])[-1])
+    #sentence = "patrick sebastien is a very young and handsome person"
+    #print(sorted(frequence(sentence).items(), key = lambda x: x[1])[-1])
+
+    draw_tree()
+    exitonclick()
+
+
